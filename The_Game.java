11b63578942e1a7a5bdda6d876c42840c2b3819d
cackrogt <cackrogt93@gmail.com>
2020-08-09 20:16:34 +0530

    Create The_Game.java

---
+++ The_Game.java
@@ -0,0 +1,38 @@
+package housie;
+
+import java.util.*;
+
+public class The_Game {
+	
+	
+	
+	public static void main(String[] args) {
+		//so that only one process can at a time read it.
+		List<Integer> screams  = Collections.synchronizedList(new ArrayList<Integer>());
+		List<Integer> shared  = Collections.synchronizedList(new ArrayList<Integer>());
+		List<Integer> running  = Collections.synchronizedList(new ArrayList<Integer>());
+		
+		int n = 5; //this is the number of processes; take as input
+		Moderator M = new Moderator(shared,screams,running,n );
+		
+		Player[] players = new Player[n];
+		for(int i =0; i<n; i++) {
+			players[i] = new Player(i,shared,screams,running);
+		}
+		Thread[] threads = new Thread[n];
+		
+		for(int i =0; i<n; i++) {
+			threads[i] = new Thread(players[i]);
+		}
+		 
+		Thread Mod = new Thread(M);
+		
+		for(int i =0; i<n; i++) {
+			threads[i].start();
+		}
+		
+		Mod.start();
+		
+	}
+	
+}
